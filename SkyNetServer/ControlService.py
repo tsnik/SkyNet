f0f0ed10be9a8a2570a55a78b9ed997f10f801d2
Nikita Tsyrlin <nstsyrlin@gmail.com>
2016-04-01 18:31:45 +0300

    Start ControlService based on conf file

--- SkyNetServer/ControlService.py
+++ SkyNetServer/ControlService.py
@@ -1,16 +1,18 @@
 from snp import SNPService, SNProtocolServerFactory
 from twisted.application import internet
-
+from DB import DB
 
 class ControlService(SNPService):
 
     def __init__(self, config):
         self.factory = SNProtocolServerFactory(self)
         self.config = config
-        self.port = config.port
+        self.port = int(config.port)
         self.iface = config.iface
+        self.db = DB.get_db()
         self.controllers = {}
-        self.ControlServer = internet.TCPServer(self.port, self.factory, self.iface)
+        print(self.port)
+        self.ControlServer = internet.TCPServer(self.port, self.factory, interface=self.iface)
 
     def connectionMade(self, protocol):
         #TODO: Request to BD
