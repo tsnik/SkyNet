eb4afa9ad0d8a1dd836c7fcf389f43b8de8c7d2d
Nikita Tsyrlin <nstsyrlin@gmail.com>
2016-04-01 18:30:26 +0300

    Start reactor in main file

--- SkyNetServer/server.py
+++ SkyNetServer/server.py
@@ -1,3 +1,9 @@
+from twisted.internet import reactor
+from twisted.application import service
 from MainService import MainService
 
 a = MainService()
+application = service.Application("SkyNet")
+a.setServiceParent(application)
+a.startService()
+reactor.run()
