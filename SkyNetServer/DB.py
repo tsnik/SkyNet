5473435f14486bb055e7fc26c5b49652c61198a0
Nikita Tsyrlin <nstsyrlin@gmail.com>
2016-04-03 21:14:21 +0300

    Create methods for saving loading scripts from db

--- SkyNetServer/DB.py
+++ SkyNetServer/DB.py
@@ -1,4 +1,7 @@
+import pickle
 from twisted.enterprise import adbapi
+
+
 class DB:
     filename = "skynet.cfg"
     _db = None
@@ -15,16 +18,18 @@ class DB:
     @staticmethod
     def init_db(txn):
         txn.execute('''CREATE TABLE IF NOT EXISTS DeviceServers
-             (id INTEGER PRIMARY KEY, ip TEXT, port INTEGER, name TEXT)''')
+              (id INTEGER PRIMARY KEY, ip TEXT, port INTEGER, name TEXT)''')
         txn.execute('''CREATE TABLE IF NOT EXISTS ControlServers
-             (id INTEGER PRIMARY KEY, ip TEXT, name TEXT)''')
+              (id INTEGER PRIMARY KEY, ip TEXT, name TEXT)''')
         txn.execute('''CREATE TABLE IF NOT EXISTS Methods
-             (id INTEGER PRIMARY KEY, name TEXT, control_server INTEGER)''')
+              (id INTEGER PRIMARY KEY, name TEXT, control_server INTEGER)''')
         txn.execute('''CREATE TABLE IF NOT EXISTS Devices
-             (id INTEGER PRIMARY KEY, name TEXT, device_server INTEGER,
+              (id INTEGER PRIMARY KEY, name TEXT, device_server INTEGER,
               device_id TEXT)''')
         txn.execute('''CREATE TABLE IF NOT EXISTS RawData
-             (id INTEGER PRIMARY KEY, device INTEGER , field TEXT, value TEXT)''')
+              (id INTEGER PRIMARY KEY, device INTEGER , field TEXT, value TEXT)''')
+        txn.execute('''CREATE TABLE IF NOT EXISTS Scripts
+              (id INTEGER PRIMARY KEY, script TEXT)''')
 
     @staticmethod
     def _init_db_callback(res):
@@ -70,3 +75,27 @@ class DB:
         txn.execute('''SELECT value FROM RawData WHERE devid = ? AND field = ? ORDER BY id DESC''', devid, field)
         return txn.fetchone()[0]
 
+    @staticmethod
+    def add_script(script):
+        db = DB.get_db()
+        s = pickle.dumps(script)
+        return db.runInteraction(DB._add_script, s)
+
+    @staticmethod
+    def _add_script(txn, s):
+        DB._check_db_ready()
+        txn.execute('''INSERT INTO Scripts (script) VALUES (?)''', s)
+
+    @staticmethod
+    def get_scripts():
+        db = DB.get_db()
+
+    @staticmethod
+    def _get_scripts(txn):
+        DB._check_db_ready()
+        txn.execute('''SELECT * FROM Scripts''')
+        scripts = {}
+        ss = txn.fetchall()
+        for s in ss:
+            scripts[s[0]] = pickle.loads(s[1])
+        return scripts
