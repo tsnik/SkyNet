784641f6925aa1cc983cebdcf736cbeedc5c61cf
Nikita Tsyrlin <nstsyrlin@gmail.com>
2016-04-05 21:39:44 +0300

    Move script file from SkyNetServer to snp module

--- snp/__init__.py
+++ snp/__init__.py
@@ -2,3 +2,4 @@ from snp.SNProtocol import SNProtocol
 from snp.SNProtocolClientFactory import SNProtocolClientFactory
 from snp.SNProtocolServerFactory import SNProtocolServerFactory
 from snp.SNPService import SNPService
+from snp.Script import Script
