44bf2017f7e1f6d6a8aa8f558245ba44f63e01f0
Nikita Tsyrlin <nstsyrlin@gmail.com>
2016-04-06 17:32:56 +0300

    Create driver template

---
+++ SkyNetDeviceServer/drivers/SimpleDriver.py
@@ -0,0 +1,21 @@
+from FieldInfo import FieldInfo
+from twisted.internet import defer
+
+
+class SimpleDriver:
+    def __init__(self, updated):
+        self.updated = updated
+
+    def get_device_fields(self):
+        return defer.maybeDeferred({})
+
+    def update_device_field(self, field_name, value):
+        def callb(res):
+            for field in self.fields:
+                if field.Writable and field.Name == field_name:
+                    up_func = getattr(self, "update_{0}".format(field_name))
+                    return up_func(value)
+            raise ValueError("Wrong FieldName")
+        d = self.get_device_fields()
+        d.addCallback(callb)
+        return d
