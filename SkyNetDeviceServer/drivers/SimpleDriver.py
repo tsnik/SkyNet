59633101ff419783db55cc8a45966d6ca6757ecb
Nikita Tsyrlin <nstsyrlin@gmail.com>
2016-04-06 17:43:19 +0300

    Additional checks in SimpleDriver

--- SkyNetDeviceServer/drivers/SimpleDriver.py
+++ SkyNetDeviceServer/drivers/SimpleDriver.py
@@ -12,10 +12,12 @@ class SimpleDriver:
     def update_device_field(self, field_name, value):
         def callb(res):
             for field in self.fields:
-                if field.Writable and field.Name == field_name:
-                    up_func = getattr(self, "update_{0}".format(field_name))
+                if field.Writable and field.Name == field_name and type(field.Value) == type(value):
+                    up_func = getattr(self, "update_{0}".format(field_name.lower()))
+                    if up_func is None:
+                        raise RuntimeError("No method to handle update")
                     return up_func(value)
-            raise ValueError("Wrong FieldName")
+            raise ValueError("Wrong field passed")
         d = self.get_device_fields()
         d.addCallback(callb)
         return d
